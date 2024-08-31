0e469c960b499515bc3137d78e4159daa7b963b6
Manu K N <201910101325@presidencyuniversity.in>
2024-08-31 17:18:08 +0530

    finished quick sort

---
+++ QuickSort.js
@@ -0,0 +1,34 @@
+function pivot(arr, startindex = 0, endindex = arr.length - 1) {
+  if (!arr.length) return 0;
+  
+  let pivotElement = arr[startindex];
+  let pivotIndex = startindex;
+
+  for (let i = startindex + 1; i <= endindex; i++) {
+    if (arr[i] < pivotElement) {
+      pivotIndex++;
+      swap(arr, pivotIndex, i);
+    }
+  }
+
+  swap(arr, startindex, pivotIndex); 
+  return pivotIndex; 
+}
+   
+   function swap(arr , i  , j) {
+       [arr[i], arr[j]] = [arr[j],arr[i]]
+   }
+   
+   function QuickSort(arr , low = 0 , high = arr.length - 1){
+    if(low < high){
+     let pivotindex = pivot(arr ,low , high)
+     //left
+     QuickSort(arr , low , pivotindex - 1);
+     //right
+     QuickSort(arr , pivotindex + 1 , high)
+    }
+    return arr 
+ }
+ const arr1 = [100,4,8,2,1,5,7,6,3]
+ console.log(QuickSort(arr1))
+
