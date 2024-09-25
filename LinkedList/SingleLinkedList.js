285354feaf56b18670a14f7a2efc989a8ea434f1
Manu K N <201910101325@presidencyuniversity.in>
2024-09-25 23:26:17 +0530

    shift and unshift

--- LinkedList/SingleLinkedList.js
+++ LinkedList/SingleLinkedList.js
@@ -41,14 +41,32 @@ class SingleLinkedList {
     }
     return current;
   }
+  shift(){
+    if(!this.head) return undefined
+    let removedHead = this.head
+     this.head = removedHead.next
+     this.length--
+     if(this.length === 0)
+     {
+      this.tail = null
+     }
+     return removedHead
+  }
+  unshift(val){
+    var newnode = new Node(val)
+    if(!this.head) {
+      this.head = newnode
+      this.tail = newnode
+    }
+    else{
+      newnode.next = this.head
+      this.head = newnode
+    }
+    this.length++
+    return this
+  }
 }
 
 const list = new SingleLinkedList();
 
-console.log(list.push('Hello'));
-console.log(list.push('Bingo'));
-
-console.log(list.pop());
-console.log(list.pop());
-
-console.log(list);
+console.log(list.unshift("manu"))
