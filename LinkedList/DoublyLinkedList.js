f88d130598f45f2d51133620e7304da6a4b07dd8
Manu <manu.kn@myn.team>
2024-10-01 18:06:11 +0530

    completed till Get in DLL

--- LinkedList/DoublyLinkedList.js
+++ LinkedList/DoublyLinkedList.js
@@ -35,9 +35,57 @@ class DoublyLinkedList {
     } else {
       this.tail = removeNode.prev;
       this.tail.next = null;
+      removeNode.prev = null;
     }
     this.length--;
     return removeNode;
+  }
+  shift() {
+    if (!this.tail) return false;
+    let removedHead = this.head;
+    if (this.length === 1) {
+      this.tail = null;
+      this.tail = null;
+    } else {
+      this.head = removedHead.next;
+      this.head.prev = null;
+      removedHead.next = null;
+    }
+    this.length--;
+    return removedHead;
+  }
+  unshift(val) {
+    let newnode = new Node(val);
+    if (!this.head) {
+      this.head = newnode;
+      this.tail = newnode;
+    } else {
+      newnode.next = this.head;
+      this.head.prev = newnode;
+      this.head = newnode;
+    }
+    this.length++;
+    return this;
+  }
+  Get(index) {
+    if (!this.length || index < 0 || index > this.length - 1) return null;
+    else if (index > this.length / 2) {
+      let count = this.length - 1;
+      let prevVal = this.tail;
+      while (count != index) {
+        prevVal = prevVal.prev;
+        count--;
+      }
+      return prevVal.val;
+    } else {
+      let count = 0;
+      let nextVal = this.head;
+      while (count != index) {
+        nextVal = nextVal.next;
+        count++;
+      }
+      return nextVal.val;
+    }
   }
 }
 let Dlist = new DoublyLinkedList();
@@ -45,4 +93,7 @@ console.log(Dlist.Push('Manu'));
 console.log(Dlist.Push('Manoj'));
 console.log(Dlist.Push('Vinay'));
 console.log(Dlist.Push('Mithun'));
-console.log(Dlist.Pop());
+console.log(Dlist.shift());
+console.log(Dlist.unshift('chethu'));
+console.log(Dlist.Get(2));
+console.log(Dlist);
