402fad5956ab99d39a523737eb49945671123e7c
Manu <manu.kn@myn.team>
2024-11-04 12:08:15 +0530

    added max elemnet

--- javascriptPlayground.js/play1.js
+++ javascriptPlayground.js/play1.js
@@ -9,3 +9,16 @@ const object2 = {};
 Object.assign(object2, object1);
 
 console.log(object2);
+
+function playfground(arr) {
+  if (!arr.length) return 'Empty Array';
+  let largestNo = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largestNo) {
+      largestNo = arr[i];
+    }
+  }
+  return largestNo;
+}
+
+console.log(playfground([ 114, 3, 65, 7, 89, 69 , 100]));
